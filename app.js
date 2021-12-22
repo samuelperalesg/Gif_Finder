a7bb0abc99c11d1fe365f3521a68fbbe0fdb5e17
s.pera6@gmail.com <s.pera6@gmail.com>
2021-12-22 10:56:40 -0700

    add element ref js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 // constants
 const URL = `https://api.giphy.com/v1/gifs/search?api_key=nINz5xXnq2CwNwZIhFlSaBI6KdAadvqt&q=`
+
+// element references
+const $form = $('form');
+const $input = $('input[type="text"]');
+
+$form.on('submit', getGiphy);
