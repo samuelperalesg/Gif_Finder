30e1d35a6bef19d1bd3b281e8812702af08bf53e
s.pera6@gmail.com <s.pera6@gmail.com>
2021-12-22 10:58:32 -0700

    add getGiphy func

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ const $form = $('form');
 const $input = $('input[type="text"]');
 
 $form.on('submit', getGiphy);
+
+// functions
+function getGiphy(evt) {
+  evt.preventDefault();
+
+  const userInput = $input.val()
+
+  if(!userInput) return
+
+  $.ajax(URL + userInput).then(function(data) {
+    render(data)
+  
+  },  function(error) {
+    return (error, 'something went wrong')
+  })
+}
